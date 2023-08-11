6fd4c52f274ebdde7c14ba75153763460312bf24
Gareth Coles <gdude2002@gmail.com>
2023-08-11 17:28:16 +0100

    Switch to version catalog

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,15 +17,11 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-cli:0.3.5")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
+    implementation(libs.kotlinx.cli)
+    implementation(libs.kotlinx.serialization)
 
-    implementation("com.fasterxml.jackson.core:jackson-core:2.15.2")
-    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.15.2")
-    implementation("io.github.oshai:kotlin-logging-jvm:5.1.0")
-    implementation("org.apache.logging.log4j:log4j-core:2.20.0")
-    implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.20.0")
-    implementation("org.fusesource.jansi:jansi:2.4.0")
+    implementation(libs.logging)
+    implementation(libs.bundles.log4j)
 
     testImplementation(kotlin("test"))
 }
